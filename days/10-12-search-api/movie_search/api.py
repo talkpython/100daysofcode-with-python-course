7d4967714b9599f32906a65649c96138bd5f9ac2
Michael Kennedy <mikeckennedy@gmail.com>
2017-12-21 09:52:32 -0800

    Demo code from Search API.

---
+++ days/10-12-search-api/movie_search/api.py
@@ -0,0 +1,21 @@
+from typing import List
+
+import requests
+import collections
+
+Movie = collections.namedtuple('Movie', 'imdb_code, title, director, keywords, '
+                                        'duration, genres, rating, year, imdb_score')
+
+
+def find_movie_by_title(keyword: str) -> List[Movie]:
+    url = f'http://movie_service.talkpython.fm/api/search/{keyword}'
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    results = resp.json()
+    movies = []
+    for r in results.get('hits'):
+        movies.append(Movie(**r))
+
+    return movies
