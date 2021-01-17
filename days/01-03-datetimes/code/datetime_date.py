da8b69d0f9b9f83a90803f071a2b76dbc9c65494
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-16 16:36:26 -0800

    Add a quick comment about why this varies from the recording.

--- days/01-03-datetimes/code/datetime_date.py
+++ days/01-03-datetimes/code/datetime_date.py
@@ -1,6 +1,6 @@
 #!python3
 
-from datetime import date
+from datetime import date
 from datetime import datetime
 
 datetime.today()
@@ -34,6 +34,7 @@ christmas = date(today_date.year, 12, 25)
 christmas
 # datetime.date(2021, 12, 25)
 
+# We need to use != & == rather than is / is not for comparison. Sorry for the mistake in the video.
 if christmas != today_date:
     print("Sorry there are still " + str((christmas - today_date).days) + " until Christmas!")
 else:
